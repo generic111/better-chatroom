f8d0d6be1790313132863f9560187aa6826e04cf
eluo2527 <eluo2527@uni.sydney.edu.au>
2024-05-17 02:51:20 +1000

    delete friends is now a button

--- frontend/src/components/friends/Friends.jsx
+++ frontend/src/components/friends/Friends.jsx
@@ -2,13 +2,57 @@ import Conversations from "../sidebar/Conversations.jsx";
 import SignOutButton from "../sidebar/SignOutButton.jsx";
 import SearchInput from "../sidebar/SearchInput.jsx";
 import DeleteFriend from "../sidebar/DeleteFriend";
+import { Link } from "react-router-dom";
+
+
+// import Article from "./Article.jsx";
+import useGetArticles from "../../hooks/useGetArticles";
+import useCreateArticle from "../../store/useCreateArticlePage.js";
+import useArticle from "../../store/useArticle";
+import useListenArticles from "../../hooks/useListenArticles.js";
+import { useAuthContext } from "../../context/AuthContext.jsx";
+import toast from "react-hot-toast";
+
 
 const Friends = () => {
+
+    const {loading, articles} = useGetArticles();
+    const {selectedArticle, setSelectedArticle} = useArticle();
+    const {selectedCreateNewArticle, setSelectedEditArticle, setSelectedCreateNewArticle} = useCreateArticle();
+    // console.log("yahhh ", conversations);
+    const {authUser} = useAuthContext();
+
+    useListenArticles();
+
+    const handleClick = () => {
+        console.log(authUser);
+        if (authUser.muted) {
+            toast.error("You are muted and cannot create articles");
+            return;
+        }
+
+        setSelectedCreateNewArticle(true); 
+        setSelectedArticle(null); 
+        setSelectedEditArticle(null);
+    }
+
+
     return (
         <div className="border-r border-slate-500 p-4 flex flex-col"> 
             <SearchInput />
-            <>...</>
-            <DeleteFriend />
+
+            {/* <div className="flex justify-center mb-5">
+                <button className={"bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"}
+                onClick={handleClick}>
+                    Create Conversation
+                </button>
+            </div>
+            <div className="flex justify-center mb-5">
+                <Link to="/signin" className="text-blue-600">
+                    Don't have an account? Sign Up
+                </Link>
+            </div> */}
+
             <div className = 'divider px-3'></div>
             <Conversations /> 
             <SignOutButton /> 
--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -1,5 +1,6 @@
 import { useSocketContext } from "../../context/SocketContext";
 import useConversation from "../../store/useConversation";
+import useSendDeleteRequest from '../../hooks/useSendDeleteRequest.js';
 
 
 const Conversation = ({conversation}) => {
@@ -9,7 +10,14 @@ const Conversation = ({conversation}) => {
     const isSelected = selectedConversation?._id === conversation._id;
     const {onlinePeeps} = useSocketContext();
     const isOnline = onlinePeeps.includes(conversation._id);
+    const {loading, sendRequest} = useSendDeleteRequest();
 
+    const handleClick = () => {
+
+        console.log(conversation.username);
+        sendRequest(conversation.username)
+
+    }
 
     return (<>
 			<div className={`flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer
@@ -24,7 +32,13 @@ const Conversation = ({conversation}) => {
             <div className="flex flex-col flex-1">
                 <div className = 'flex gap-3 justify-between'>
                     <p className = 'font-bold text-black'>{conversation.username}</p>
-                    <span className = 'text-xl'> 💀 </span>
+                    {/* <span className = 'text-xl'> 💀 </span> */}
+
+                        <button className={"hover:bg-red-700 text-white font-bold py-2 px-4 rounded"}
+                        onClick={handleClick}>
+                            💀
+                        </button>
+               
                 </div>
             </div>
         </div>
