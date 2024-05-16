d19cf01b7c772b6f9e4b7db2dfbf2eed50d6c815
eluo2527 <eluo2527@uni.sydney.edu.au>
2024-05-17 02:56:52 +1000

    removed code

--- frontend/src/components/friends/Friends.jsx
+++ frontend/src/components/friends/Friends.jsx
@@ -1,41 +1,8 @@
 import Conversations from "../sidebar/Conversations.jsx";
 import SignOutButton from "../sidebar/SignOutButton.jsx";
 import SearchInput from "../sidebar/SearchInput.jsx";
-import DeleteFriend from "../sidebar/DeleteFriend";
-import { Link } from "react-router-dom";
-
-
-// import Article from "./Article.jsx";
-import useGetArticles from "../../hooks/useGetArticles";
-import useCreateArticle from "../../store/useCreateArticlePage.js";
-import useArticle from "../../store/useArticle";
-import useListenArticles from "../../hooks/useListenArticles.js";
-import { useAuthContext } from "../../context/AuthContext.jsx";
-import toast from "react-hot-toast";
-
 
 const Friends = () => {
-
-    const {loading, articles} = useGetArticles();
-    const {selectedArticle, setSelectedArticle} = useArticle();
-    const {selectedCreateNewArticle, setSelectedEditArticle, setSelectedCreateNewArticle} = useCreateArticle();
-    // console.log("yahhh ", conversations);
-    const {authUser} = useAuthContext();
-
-    useListenArticles();
-
-    const handleClick = () => {
-        console.log(authUser);
-        if (authUser.muted) {
-            toast.error("You are muted and cannot create articles");
-            return;
-        }
-
-        setSelectedCreateNewArticle(true); 
-        setSelectedArticle(null); 
-        setSelectedEditArticle(null);
-    }
-
 
     return (
         <div className="border-r border-slate-500 p-4 flex flex-col"> 
