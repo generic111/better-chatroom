014fcf5f81a8dfa1c79ab9485248ca55cf1a117b
Eric <ericxieau@gmail.com>
2024-05-18 23:23:22 +1000

    local version

--- backend/server.js
+++ backend/server.js
@@ -35,11 +35,11 @@ app.use("/api/friends", friendRoutes);
 app.use("/api/articles", articleRoutes);
 app.use("/api/chat", chatRoutes);
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
+// app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-});
+// app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+// });
 
 server.listen(PORT, () => {
     connectToMongoDB();
